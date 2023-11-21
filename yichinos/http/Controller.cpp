671b597b2adb9a8ef59ccffb4cd62453fb4059d5
yichinos <ichinose.0720.emt@gmail.com>
2023-11-21 15:23:16 +0900

    11/21

--- yichinos/http/Controller.cpp
+++ yichinos/http/Controller.cpp
@@ -13,52 +13,61 @@ bool isCgiScript(const std::string& filePath) {
     return filePath.size() >= 4 && filePath.substr(filePath.size() - 4) == ".cgi";
 }
 
-std::string executeCgiScript(const std::string& filePath) 
+void executeCgiScript(Request& req, Response& res) 
 {
+    std::string path = req.getUri();
+    if (access(path.c_str(), F_OK | X_OK) != 0) 
+    {
+        res.setStatus("404 Not Found");
+        res.setBody("<html><body><h1>404 Not Found</h1><p>Requested script not found.</p></body></html>");
+        return;
+    }
     int pipefd[2];
-    pipe(pipefd);  // パイプの作成
-
+    pipe(pipefd);
     pid_t pid = fork();
-
-    if (pid == 0) {
-        // 子プロセス
-        close(pipefd[0]);  // 読み込み側を閉じる
-        dup2(pipefd[1], STDOUT_FILENO);  // 標準出力をパイプに付け替え
-        dup2(pipefd[1], STDERR_FILENO);  // 標準エラーもパイプに付け替え
+    if (pid == 0) //child
+    {
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO); 
+        dup2(pipefd[1], STDERR_FILENO);
         close(pipefd[1]);
-
-        // 外部プログラムの実行
-        execve(filePath.c_str(), NULL, NULL);
-        exit(EXIT_FAILURE);
-    } else if (pid > 0) {
-        // 親プロセス
-        close(pipefd[1]);  // 書き込み側を閉じる
+        execve(path.c_str(), NULL, NULL);
+        exit(500);
+    }
+    else if (pid > 0) 
+    {
+        close(pipefd[1]);
+        int status;
         std::string output;
         char buf[1024];
         ssize_t len;
         while ((len = read(pipefd[0], buf, sizeof(buf) - 1)) > 0) {
-            buf[len] = '\0';  // 子プロセスの出力を表示
+            buf[len] = '\0';
             output += buf;
         }
         close(pipefd[0]);
-        waitpid(pid, NULL, 0);
-        return output;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status))
+        {
+            int exitStatus = WEXITSTATUS(status);
+            if (exitStatus == 500) 
+            {
+                res.setStatus("500 Internal Server Error");
+                res.setBody("");
+                return;
+            }
+        }
+        res.setStatus("200 OK");
+        res.setBody(output);
+        return;
     }
-    return "";
 }
 
 void Controller::processFile(Request& req, Response& res) 
 {
     if (isCgiScript(req.getUri())) {
-        // CGIスクリプトの実行と出力のキャプチャ
-        res.setBody(executeCgiScript(req.getUri()));
-        if (res.getBody().empty()) {
-            res.setStatus("404 Not Found");
-        } else {
-            res.setStatus("200 OK");
-        }
+        executeCgiScript(req, res);
     } else {
-        // ファイルの読み込み
         res.setStatus(openFile(req.getUri()));
         res.setBody(getBody(res.getStatus(), req.getUri()));
     }
