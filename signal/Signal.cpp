c92f151287ff905f508be45181fc38159fa0a74a
Shota Nemoto <snemoto@c1r20s9.42tokyo.jp>
2023-12-31 12:06:42 +0900

    SIGTERMのみ受付 #17

--- signal/Signal.cpp
+++ signal/Signal.cpp
@@ -3,10 +3,11 @@
 // デフォルト動作でプログラムを終了するシグナルを無視する
 void ignore_sig(void)
 {
-	// signal(SIGINT, SIG_IGN);
 	// signal(SIGTERM, SIG_IGN);
-	// signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 	signal(SIGPIPE, SIG_IGN);
+	signal(SIGHUP, SIG_IGN);
 	signal(SIGALRM, SIG_IGN);
 	signal(SIGUSR1, SIG_IGN);
 	signal(SIGUSR2, SIG_IGN);
--- signal/Signal.hpp
+++ signal/Signal.hpp
@@ -1,9 +1,3 @@
-// デフォルト動作でプログラムを終了するシグナルを無視する
-// SIGINT : ctrl+c
-// SIGPIPE : 接続先がないパイプやソケットの書き込み
-// SIGALRM : alarm(2)からのタイマー
-// SIGTERM/SIGUSR1/SIGUSR2 : killコマンド
-
 #include <signal.h>
 
 void ignore_sig(void);
