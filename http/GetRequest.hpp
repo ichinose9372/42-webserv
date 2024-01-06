f9f3c79c2c7cf010d52d1f5c4e613d79532dad2e
welove-t <tlove.tky@gmail.com>
2024-01-06 11:59:17 +0900

    openFileのファイルの存在と読み取り許可の確認処理を追加

--- http/GetRequest.hpp
+++ http/GetRequest.hpp
@@ -3,18 +3,17 @@
 
 #include "Controller.hpp"
 #include "ExecCgi.hpp"
-// #include <sys/stat.h>
-
+#include <sys/stat.h>
 
 class GetRequest
 {
-    public:
-        GetRequest();
-        ~GetRequest();
-        static void handleGetRequest(Request& req, Response& res);
-        static void executeCgiScript(Request& req, Response& res);
-        static std::string openFile(const std::string& filePath);
-        static std::string getBody(const std::string& status, const std::string& filePath);
+public:
+    GetRequest();
+    ~GetRequest();
+    static void handleGetRequest(Request &req, Response &res);
+    static void executeCgiScript(Request &req, Response &res);
+    static std::string openFile(const std::string &filePath);
+    static std::string getBody(const std::string &status, const std::string &filePath);
 };
 
 #endif
