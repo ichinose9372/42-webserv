75faa20401fef8cbbaf3984da4b1fb8d75a5f34a
welove-t <tlove.tky@gmail.com>
2024-01-07 12:12:51 +0900

    ステータスコードのマッピング化 #38

--- http/Response.cpp
+++ http/Response.cpp
@@ -4,6 +4,24 @@ Response::Response() {}
 
 Response::~Response() {}
 
+// ステータスコードとステータスメッセージのマップを定義
+const std::map<int, std::string> STATUS_CODES = {
+    {200, "200 OK"},
+    {403, "403 Forbidden"},
+    {404, "404 Not Found"},
+    {500, "500 Internal Server Error"}
+    // 他のステータスコードも必要に応じて追加
+};
+
+const std::string getStatusMessage(int statusCode)
+{
+    std::map<int, std::string>::const_iterator it = STATUS_CODES.find(statusCode);
+    if (it != STATUS_CODES.end())
+        return it->second;
+    else
+        return STATUS_CODES.find(500)->second;
+}
+
 void Response::setResponse()
 {
     response = "HTTP/1.1 ";
@@ -13,9 +31,9 @@ void Response::setResponse()
     response += "\r\n";
     response += body;
     // std::cout << response << std::endl;
-}  
+}
 
-void Response::setStatus(const std::string& status)
+void Response::setStatus(const std::string &status)
 {
     this->status = status;
 }
@@ -24,7 +42,7 @@ std::string Response::allgetHeader()
 {
     std::string header;
     std::map<std::string, std::string>::iterator it;
-    for(it = headers.begin(); it != headers.end(); it++)
+    for (it = headers.begin(); it != headers.end(); it++)
     {
         header += it->first;
         header += it->second;
@@ -33,12 +51,12 @@ std::string Response::allgetHeader()
     return header;
 }
 
-void Response::setBody(const std::string& body)
+void Response::setBody(const std::string &body)
 {
     this->body = body;
 }
 
-void Response::setHeaders(const std::string& key, const std::string& value)
+void Response::setHeaders(const std::string &key, const std::string &value)
 {
     headers[key] = value;
 }
