1465504e1bd8462a53850c04cad286acff563e58
welove-t <tlove.tky@gmail.com>
2024-01-11 14:03:54 +0900

    GetRequest内の未使用関数や不要コメントの削除

--- http/GetRequest.cpp
+++ http/GetRequest.cpp
@@ -38,21 +38,6 @@ int GetRequest::openFile(const std::string &filePath)
     if (!file)
         return 500;
     return 200;
-    // 以降の行は削除する。この関数ではファイルのアクセシビリティに関するチェックのみとする
-    // std::string content;
-
-    // if (file.is_open())
-    // {
-    //     content = "200 OK";
-    //     file.close();
-    // }
-    // else
-    // {
-    //     content = "404 Not Found";
-    // }
-
-    // std::cout << "content = " << content << std::endl;
-    // return content;
 }
 
 std::string GetRequest::getBody(const std::string &filePath)
@@ -76,61 +61,8 @@ std::string GetRequest::getBody(const std::string &filePath)
     }
     file.close();
 
-    std::cout << "Read content size: " << buffer.str().size() << " characters." << std::endl;
+    // std::cout << "Read content size: " << buffer.str().size() << " characters." << std::endl;
     return buffer.str();
-}
-
-void GetRequest::executeCgiScript(Request &req, Response &res)
-{
-    std::string path = req.getUri();
-    if (access(path.c_str(), F_OK | X_OK) != 0)
-    {
-        res.setStatus("404 Not Found");
-        res.setBody("<html><body><h1>404 Not Found</h1><p>Requested script not found.</p></body></html>");
-        return;
-    }
-    int pipefd[2];
-    pipe(pipefd);
-    pid_t pid = fork();
-    if (pid == 0) // child
-    {
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        dup2(pipefd[1], STDERR_FILENO);
-        close(pipefd[1]);
-        execve(path.c_str(), NULL, NULL);
-        exit(500);
-    }
-    else if (pid > 0)
-    {
-        close(pipefd[1]);
-        int status;
-        std::string output;
-        char buf[1024];
-        ssize_t len;
-        while ((len = read(pipefd[0], buf, sizeof(buf) - 1)) > 0)
-        {
-            buf[len] = '\0';
-            output += buf;
-        }
-        close(pipefd[0]);
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status))
-        {
-            int exitStatus = WEXITSTATUS(status);
-            if (exitStatus == 500)
-            {
-                res.setStatus("500 Internal Server Error");
-                res.setBody("");
-                return;
-            }
-        }
-        res.setStatus("200 OK");
-        res.setHeaders("Content-Type: ", "text/html");
-        res.setBody(output);
-        res.setHeaders("Content-Length: ", std::to_string(output.size()));
-        return;
-    }
 }
 
 void GetRequest::handleGetRequest(Request &req, Response &res)
@@ -142,12 +74,6 @@ void GetRequest::handleGetRequest(Request &req, Response &res)
     }
     else
     {
-        // if (req.getFilepath() != "")
-        // {
-        //     std::cout << "-------------handleget filepath != 0 --------------- \n";
-        //     res.setStatus(openFile(req.getFilepath()));
-        // }
-        // else
         const std::string message = res.getStatusMessage(openFile(req.getUri()));
         res.setStatus(message);
         res.setHeaders("Content-Type: ", "text/html");
