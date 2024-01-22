b1d5f276111e04a6f912a099fdd4f96f6b692192
Shota Nemoto <snemoto@c1r14s8.42tokyo.jp>
2024-01-22 10:08:22 +0900

    再帰によるエントリ処理 #87

--- docs/autoindex/app.py
+++ docs/autoindex/app.py
@@ -2,42 +2,78 @@
 # -*- coding: utf-8 -*-
 
 import os
-# print(os.environ.get('PATH'))
-# print(os.environ.get('PYTHONPATH'))
-
-# import sys
-# print(sys.path)
-
 from flask import Flask, render_template
 
+docs_path = os.getcwd() + "/docs"
+
 app = Flask(__name__)
-app.template_folder = os.path.join(os.path.dirname(__file__), "templates")
 
-def get_directory_contents():
-	try:
-		path = os.getcwd()
-		target = "/autoindex"
+# @app.route('/show_file/<path:path>')
+# def show_file(path):
+# 		full_path = os.path.join(docs_path, path)
+# 		return render_template(full_path)
 
-		if (path.endswith(target)):
-			path = path[:-len(target)]
-
-		files = []
-		directories = []
-		entries = os.listdir(path)
-
-		for entry in entries:
-			full_path = os.path.join(path, entry)
-			if os.path.isdir(full_path):
-				directories.append(entry)
-			else:
-				files.append(entry)
-		
-		with app.app_context():
-			return render_template("autoindex.html", files=files, directories=directories)
+# @app.route('/')
+def get_docs_contents():
+	list_files, list_pathes = get_entries(docs_path)
+	combined_data = list(zip(list_files, list_pathes))
+	with app.app_context():
+		return render_template("autoindex.html", combined_data=combined_data)
 				
-	except OSError as e:
-		print("Error reading directory: {}".format(e))
-		return []
+def get_entries(docs_path):
+	list_files = []
+	list_pathes = []
+
+	def process_entry(entry):
+		full_path = os.path.join(docs_path, entry)
+		if (os.path.isdir(full_path)):
+			dir_entries = os.listdir(full_path)
+			for dir_entry in dir_entries:
+				process_entry(os.path.join(entry, dir_entry))
+		else:
+			list_files.append(entry)
+			list_pathes.append(full_path)
+
+	entries = os.listdir(docs_path)
+	for entry in entries:
+		process_entry(entry)
+	return list_files, list_pathes
+
+if __name__ == '__main__':
+	print(get_docs_contents()) 
+	# app.run()
+
+
+
+
+# app.template_folder = os.path.join(os.path.dirname(__file__), "templates")
+# def get_docs_contents():
+# 	try:
+# 		docs_path = os.getcwd()
+# 		docs_path += "/docs"
+# 		entries = os.listdir(docs_path)
+# 		list_files = []
+# 		list_pathes = []
+
+# 		for entry in entries:
+# 			full_path = os.path.join(docs_path, entry)
+# 			if os.path.isdir(full_path):
+# 				dir_entries = os.listdir(full_path)
+# 				for entry in dir_entries:
+# 					list_files.append(entry)
+# 					list_pathes.append(os.path.join(full_path, entry))
+# 			else:
+# 				list_files.append(entry)
+# 				list_pathes.append(full_path)
+		
+# 		with app.app_context():
+# 			return render_template("autoindex.html",
+# 						  			files=list_files,
+# 									pathes=list_pathes)
+				
+# 	except OSError as e:
+# 		print("Error reading directory: {}".format(e))
+# 		return []
 
 		# # return files, directories
 
@@ -70,7 +106,18 @@ def get_directory_contents():
 # 	except OSError as e:
 # 		print("Error reading directory: {}".format(e))
 
+# from flask_autoindex import Autoindex
+
+# Autoindex(app, browse_root = os.path.curdir)
+
 # #---test---
+# target = "/autoindex"
+# if (path.endswith(target)):
+# 	path = path[:-len(target)]
+# print(os.environ.get('PATH'))
+# print(os.environ.get('PYTHONPATH'))
+# import sys
+# print(sys.path)
 # files, directories = getDirectoryContents()
 # print("Files: ")
 # for file in files:
@@ -78,4 +125,3 @@ def get_directory_contents():
 # print("\nDirectories: ")
 # for directory in directories:
 # 	print(directory)
-print(get_directory_contents())
