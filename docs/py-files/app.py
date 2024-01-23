1b5039be2fc8643164b769a6463ad8ca1b2a03b9
Shota Nemoto <snemoto@c1r15s9.42tokyo.jp>
2024-01-23 09:25:44 +0900

    handling method for python #92

--- docs/py-files/app.py
+++ docs/py-files/app.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import cgi
 import codecs
+import os
 
-form = cgi.FieldStorage()
+method = os.getenv('REQUEST_METHOD')
 
+html = "<html><body><p>Unsupported request method.</p></body><html>"
+if method == "GET":
+	html = codecs.open('./docs/py-files/get.html', 'r', 'utf-8').read()
+elif method == "POST":
+	html = codecs.open('./docs/py-files/post.html', 'r', 'utf-8').read()
+
+# print(method)
+print(html)
+
+# import cgi
+# form = cgi.FieldStorage()
 # 初回ロード時
 # if form.list == []:
-html = codecs.open('./docs/py-files/view_001.html', 'r', 'utf-8').read()
+# html = codecs.open('./docs/py-files/view_001.html', 'r', 'utf-8').read()
 # # SUBMITボタン押下時
 # else:
 #     html = codecs.open('./docs/py-files/view_002.html', 'r', 'utf-8').read()
-
-print(html)
