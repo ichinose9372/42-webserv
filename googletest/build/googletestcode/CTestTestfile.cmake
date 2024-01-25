10bbd57922cdb5a9d85a48504c76cbd4e55f2550
yichinos <ichinose.0720.emt@gmail.com>
2024-01-25 15:16:19 +0900

    try3

--- googletest/build/googletestcode/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ichinoseyuuki/tokyo42/webserv/googletest/googletestcode
-# Build directory: /Users/ichinoseyuuki/tokyo42/webserv/googletest/build/googletestcode
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
